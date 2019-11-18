9ed9cb303de570eca2955bc3cfb3f534d51b433e
Florian Bachofner <florianbachofner@hotmail.com>
2019-11-18 17:04:49 +0100

    test

--- faceRecognition.py
+++ faceRecognition.py
@@ -5,7 +5,11 @@ def faceDetection(img):
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
-        cv.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
+        cv.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
     cv.imshow('img', img)
     cv.waitKey()
     cv.destroyAllWindows()
+
+img = cv.imread("test.jpg")
+
+faceDetection(img)
