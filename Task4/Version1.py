e93c4e85e6aaf2105a9e920195e64de75fb71b9b
bbiaisb <57803055+bbiaisb@users.noreply.github.com>
2019-11-20 11:28:39 +0100

    Update Version1.py

--- Task4/Version1.py
+++ Task4/Version1.py
@@ -7,22 +7,14 @@ def measureSimilarities():
     before = cv2.imread('test.jpg')
     after = cv2.imread('test_edit2.jpg')
 
-    # Convert images to grayscale
     before_gray = cv2.cvtColor(before, cv2.COLOR_BGR2GRAY)
     after_gray = cv2.cvtColor(after, cv2.COLOR_BGR2GRAY)
 
-    # Compute SSIM between two images
     (score, diff) = compare_ssim(before_gray, after_gray, full=True)
     print("Image similarity", score)
 
-    # The diff image contains the actual image differences between the two images
-    # and is represented as a floating point data type in the range [0,1]
-    # so we must convert the array to 8-bit unsigned integers in the range
-    # [0,255] before we can use it with OpenCV
     diff = (diff * 255).astype("uint8")
 
-    # Threshold the difference image, followed by finding contours to
-    # obtain the regions of the two input images that differ
     thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
     contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     contours = contours[0] if len(contours) == 2 else contours[1]
