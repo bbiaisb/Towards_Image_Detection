ed938b8acaa79e1ffee932a7ffbbcf3907477826
Dadaaeisen <57802989+Dadaaeisen@users.noreply.github.com>
2019-11-22 19:32:12 +0100

    improce the code like Maria posted on github

--- faceRecognition.py
+++ faceRecognition.py
@@ -2,18 +2,15 @@ import cv2 as cv
 
 def faceDetection(img):
     face_cascade = cv.CascadeClassifier('haarcascade_frontalface_default.xml')
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+    gray = cv.cvtColor(img_2, cv.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(gray, 1.1, 4)
     for (x, y, w, h) in faces:
         cv.rectangle(img, (x, y), (x + w, y + h), (255, 255, 255), 2)
-    cv.imshow('img', img)
+    cv.imshow('img_2', img)
     cv.waitKey()
-<<<<<<< HEAD
     cv.destroyAllWindows()
-=======
-    cv.destroyAllWindows()
+
 
 img = cv.imread("test.jpg")
-
-faceDetection(img)
->>>>>>> 9ed9cb303de570eca2955bc3cfb3f534d51b433e
+img_2 = cv.imread("Haruki_Murakami.jpg")
+faceDetection(img_2)
