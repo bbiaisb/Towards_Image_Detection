a268ac78c17a6c19b1f581d529a90e8fdc23af2f
bbiaisb <57803055+bbiaisb@users.noreply.github.com>
2019-12-09 16:54:28 +0100

    fixing measurement problem

--- Task7/app/functions/task4_measureSimilarities.py
+++ Task7/app/functions/task4_measureSimilarities.py
@@ -13,7 +13,7 @@ def measureSimilarities(before, after):
 
     (score, diff) = structural_similarity(before, after, full=True) #comparing images with skimage function (Structural Similarity Index (SSIM) -> degregation of the image)
 
-    diff = (diff * 255).astype("uint8") #diff contains the differences of the image
+    #diff = (diff * 255).astype("uint8") #diff contains the differences of the image
 
     thresh = cv2.threshold(diff, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1] #finding the differences
     contours = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) #contours, CHAIN_APPROX_SIMPLE does need less memory
@@ -29,7 +29,7 @@ def measureSimilarities(before, after):
         for cI in contoursImg:
             area = cv2.contourArea(c)
             contourImg = cv2.contourArea(cI)
-            if score >= 0.5:
+            if score >= 0.3:
                 perc_score = round(score, 2)*100
                 return str(int(perc_score))+"%"
                 """
@@ -43,7 +43,8 @@ def measureSimilarities(before, after):
                         cv2.drawContours(filled_after, [c], 0, (0, 255, 0), -1)
                 """
             if area == contourImg:
-                return "Both images have no significant similarities!"
+                perc_score = round(score, 2) * 100
+                return "Both images have no significant similarities! "+str(int(perc_score)) + "%"
     perc_score = round(score, 2) * 100
     return str(int(perc_score)) + "%"
 
